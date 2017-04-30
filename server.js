0a4b0140d56c3aa83ccc9ba9cf833aef49df41c0
Aaron Kelley <Aaron Kelley>
2017-04-30 18:43:53 -0400

    debug crash

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var moment = require('moment');
 
+var port = process.env.PORT || 8080;
+
 console.log('server is starting. . .');
 
 var app = express();
 
 app.use(express.static('website'));
 
-app.listen(3000);
+app.listen(port, function(){
+
+	console.log('server is running');
+
+});
 
 app.get('/:date', showDate);
 
